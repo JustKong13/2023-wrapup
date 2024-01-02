f98fa901711ddc4ada3c426815b96cea62bea38c
Justin Kong <jkong@Justins-MacBook-Pro.local>
2024-01-02 09:30:40 -0500

    serialized data

--- src/create_json.py
+++ src/create_json.py
@@ -1,4 +1,6 @@
 import json
+import datetime as dt
+import pickle
 
 file = open('./data/data.txt')
 split_by_line = file.read().split('\n')
@@ -14,3 +16,20 @@ for i, v in enumerate(split_by_line):
 
 json_file = open('./data/data_by_date.json', 'w')
 json.dump(d, json_file)
+
+times = []
+for key in d: 
+    if d[key] == []: 
+        pass
+
+    else: 
+        for time in d[key]: 
+            month_str = key.split()[1] + '/2023'
+            time_str = time.upper()
+            dt_str = month_str + ' ' + time_str
+
+            formatted_time = dt.datetime.strptime(dt_str, '%m/%d/%Y %I:%M%p')
+            times.append(formatted_time)
+
+with open('./data/times.pkl', 'wb') as out_file:
+    pickle.dump(times, out_file)
